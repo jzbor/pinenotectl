335996e39a8f7a158181dd817d2f60faf7b761af
jzbor <jzbor@posteo.net>
2025-04-06 16:39:27 +0200

    Adding Nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Control the PineNote's special features";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+    cf.url = "github:jzbor/cornflakes";
+  };
+
+  outputs = { self, nixpkgs, cf, crane, ... }: (cf.mkLib nixpkgs).flakeForDefaultSystems (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+    craneLib = crane.mkLib pkgs;
+  in {
+    packages.default = craneLib.buildPackage {
+      src = craneLib.cleanCargoSource ./.;
+      strictDeps = true;
+    };
+
+    devShells.default = craneLib.devShell {
+      inherit (self.packages.${system}.default) name;
+
+      # Additional tools
+      nativeBuildInputs = [];
+    };
+  });
+}
